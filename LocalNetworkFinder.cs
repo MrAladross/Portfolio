82cbf3952a62b781f0fa246530425e8dc6d55ff2
MrAladross <52119895+MrAladross@users.noreply.github.com>
2022-01-04 15:40:31 -0800

    Update LocalNetworkFinder.cs

--- LocalNetworkFinder.cs
+++ LocalNetworkFinder.cs
@@ -6,8 +6,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class LocalNetworkFinder : MonoBehaviour
