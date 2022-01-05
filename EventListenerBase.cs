84a8d61d35d9a4539d2ca4b912e904cd12a34463
MrAladross <52119895+MrAladross@users.noreply.github.com>
2022-01-04 16:18:02 -0800

    Base class for event listeners.
    The abstract method allows listeners to be handled differently. The message string is populated by InvokeWithMessage in the event class.

---
+++ EventListenerBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+using UnityEditor;
+public abstract class EventListenerBase : MonoBehaviour
+{
+    [SerializeField]
+    protected GameEvent _gameEvent;
+
+    protected GameEvent.EventType _eventType;
+    public string message = "";
+    protected ScoreTracker st;
+    private void Awake()
+    {
+//        st = GameObject.FindWithTag("ScoreTracker").GetComponent<ScoreTracker>();
+        _eventType = _gameEvent.GetType();
+    }
+    private void OnEnable()
+    {
+        _gameEvent.RegisterAbstract(this);
+    }
+
+    private void OnDisable()
+    {
+        _gameEvent.DeRegisterAbstract(this);
+    }
+
+    private void OnDestroy()
+    {
+        _gameEvent.DeRegisterAbstract(this);
+    }
+    public abstract void EventMethod();
+
+}
