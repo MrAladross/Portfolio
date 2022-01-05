c7e29b9d73e3bbb022667d73f3c814b2e802553c
MrAladross <52119895+MrAladross@users.noreply.github.com>
2022-01-04 18:24:44 -0800

    Add files via upload

---
+++ NetworkSystem/ByteWord.cs
@@ -0,0 +1,17 @@
+﻿struct ByteWord
+{
+    public int Number { get; }
+    public string Word { get; }
+
+    public ByteWord(int num, string wor)
+    {
+        Number = num;
+        Word = wor;
+    }
+    
+    public override string ToString()
+    {
+        return Word + " " + Number;
+    }
+
+}
