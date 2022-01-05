709ca017f807c6cf29825590ea6623074f89797e
MrAladross <52119895+MrAladross@users.noreply.github.com>
2022-01-04 18:33:27 -0800

    Bubble pop files
    These two files are used to generate path prediction lines for the player.  A png file is provided so you can see the output.

---
+++ GridCalculator.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCalculator : MonoBehaviour
+{
+    [SerializeField] private int rowWidth;
+    [SerializeField] private int numberOfRows;
+    private List<int> positions;
+    private List<GridElement> _elements;
+    private List<bool> isChecked;
+
+[SerializeField]    private int _indexToCheck = 0;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ClearChecks();
+            CheckNeighbors(_indexToCheck);
+            //handle explosion here
+        }
+            
+    }
+
+    void ClearChecks()
+    {
+        for (int i = 0; i < isChecked.Count; ++i)
+            isChecked[i] = false;
+    }
+
+    void CheckNeighbors(int indexToCheck)
+    {
+        Debug.Log("Checking space " + indexToCheck);
+        List<int> neighbors = GetNeighbors(indexToCheck);
+        for (int i = 0; i < neighbors.Count; ++i)
+        {
+            if (!isChecked[neighbors[i]])
+                if (_elements[neighbors[i]].GetColor() == _elements[indexToCheck].GetColor()) //same color
+                {
+                    Debug.Log(neighbors[i] + " is a match.");
+                    isChecked[neighbors[i]] = true;
+                    CheckNeighbors(neighbors[i]);
+                }
+        }
+    }
+    void GenerateGrid()
+    {
+        positions = new List<int>();
+        _elements = new List<GridElement>();
+        isChecked = new List<bool>();
+        
+        for (int i = 0; i < numberOfRows; ++i)
+        {
+            for (int j = 0; j < rowWidth; ++j)
+            {
+                int index = i * rowWidth + j;
+                index = index % 2;
+                
+                
+                //positions defines neighbors and shouldn't change
+                //_elements represents balls and should change if a ball pops or moves
+                //isChecked matches size of positions
+                positions.Add(index);
+                _elements.Add(new GridElement(index%rowWidth));
+                isChecked.Add(false);
+
+            }
+        }
+
+        Debug.Log("There are " + rowWidth * numberOfRows + " rows. The list has a count of " + positions.Count);
+        Debug.Log("There are " + (positions[positions.Count - 1] +1) + " elements.");
+        
+        
+       
+    }
+
+    List<int> GetNeighbors(int index)
+    {
+        List<int> neighbors = new List<int>();
+
+
+        if ((index / rowWidth) % 2 == 0)
+        {
+            if (index - rowWidth - 1 > 0)
+            {
+                if (index % rowWidth > 0)
+                    neighbors.Add(index - rowWidth); //upleft
+
+                neighbors.Add(index - rowWidth); //upright
+            }
+
+//left
+            if (index % rowWidth > 0)
+                neighbors.Add(index - 1);
+            //right
+            if (index % rowWidth < rowWidth - 1)
+                neighbors.Add(index + 1);
+            //downleft
+            if (index + rowWidth - 1 < rowWidth * numberOfRows && index % rowWidth > 0)
+                neighbors.Add(index + rowWidth - 1);
+            //downright
+            if (index + rowWidth < rowWidth * numberOfRows)
+                neighbors.Add(index + rowWidth);
+        }
+        else
+        {
+            if (index - rowWidth > 0)
+            {
+                //upleft
+                neighbors.Add(index - rowWidth);
+                //upright
+                if ((index % rowWidth) < rowWidth - 1)
+                    neighbors.Add(index - rowWidth + 1);
+            }
+
+            //left
+            if (index % rowWidth > 0)
+                neighbors.Add(index - 1);
+            //right
+            if (index % rowWidth < rowWidth - 1)
+                neighbors.Add(index + 1);
+            //downleft
+            if (index + rowWidth < rowWidth * numberOfRows)
+                neighbors.Add(index + rowWidth);
+            //downright
+            if (index + rowWidth + 1 < rowWidth * numberOfRows && (index + rowWidth + 1) % rowWidth > 0)
+                neighbors.Add(index + rowWidth + 1);
+        }
+
+        return neighbors;
+
+    }
+
+    private void Awake()
+    {
+        GenerateGrid();
+    }
+}
+
+public class GridElement
+{
+    private int color;
+
+    public int GetColor()
+    {
+        return color;
+    }
+    public GridElement(int index)
+    {
+        color = index;
+    }
+}
---
+++ LookAtMouse.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Vector3 bubbleShooterPixelPosition;
+
+    private List<Vector3> linePositions;
+    [SerializeField] private LineRenderer lr;
+
+    [SerializeField]
+    private GameObject lineRendererObject;
+
+    private bool isBouncing = false;
+    private List<LineRenderer> bounceRenderers;
+    void Start()
+    {
+        bounceRenderers = new List<LineRenderer>();
+        bubbleShooterPixelPosition = new Vector3(Screen.width / 2, 0,0);
+        linePositions = new List<Vector3>();
+        Vector3 startPosition = Camera.main.ScreenToWorldPoint(bubbleShooterPixelPosition);
+        startPosition.z = 0;
+        linePositions.Add(startPosition);
+        linePositions.Add(Vector3.zero);
+    }
+
+    void Update()
+    {
+        isBouncing = false;
+        ClearBounceRenderers();
+        RotateCannon();
+
+        //100 is arbitrary length to represent infinity
+        Vector3 direction = transform.right.normalized * 100f;
+        
+        //left is 0,1,0
+        //right is 0,-1,0
+        //up is 1,0,0
+        direction.z = direction.x;
+        direction.x = -direction.y;
+        direction.y = direction.z;
+        direction.z = 0;
+        
+        RenderFirstLine(linePositions[0], direction);
+        
+    }
+
+    void RenderBounces(Vector3 origin, Vector3 direction)
+    {
+        //check for infinite bounces
+        if (bounceRenderers.Count > 20)
+            return;
+        
+        GameObject nextRenderer = Instantiate(lineRendererObject, origin, quaternion.identity);
+        Vector3[] positions = new Vector3[2];
+        positions[0] = origin;
+        positions[1] = direction;
+        bounceRenderers.Add(nextRenderer.GetComponent<LineRenderer>());
+        bounceRenderers[bounceRenderers.Count-1].SetPositions(positions);
+        
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            if (hit.transform.CompareTag("Wall"))
+            {
+                positions[1] = hit.point;
+                bounceRenderers[bounceRenderers.Count-1].SetPositions(positions);
+                
+                Vector3 reflection = direction;
+                reflection.x = -reflection.x;
+                RenderBounces(hit.point,reflection);
+            }
+        }
+    }
+    private void RenderFirstLine(Vector3 origin, Vector3 direction)
+    {
+        GameObject nextRenderer = Instantiate(lineRendererObject, origin, quaternion.identity);
+        Vector3[] positions = new Vector3[2];
+        positions[0] = origin;
+        positions[1] = direction;
+        bounceRenderers.Add(nextRenderer.GetComponent<LineRenderer>());
+        lr = bounceRenderers[0];
+        bounceRenderers[bounceRenderers.Count-1].SetPositions(positions);
+        
+        
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            if (hit.transform.CompareTag("Wall"))
+            {
+                lr.SetPosition(1, hit.point);
+                
+                Vector3 reflection = direction;
+                reflection.x = -reflection.x;
+                RenderBounces(hit.point,reflection);
+                isBouncing = true;
+            }
+        }
+    }
+[SerializeField]
+    private float minimumAngle = 10f;
+    private void RotateCannon()
+    {
+        mousePosition = Input.mousePosition - bubbleShooterPixelPosition;
+        float zRotation = Mathf.Clamp(Mathf.Acos(mousePosition.x / mousePosition.magnitude) * 180 / Mathf.PI - 90,-90+minimumAngle,90-minimumAngle);
+        transform.rotation = Quaternion.Euler(0, 0, zRotation);
+    }
+
+    private void ClearBounceRenderers()
+    {
+        for (int i = bounceRenderers.Count - 1; i >= 0; --i)
+        {
+            LineRenderer _renderer = bounceRenderers[i];
+            bounceRenderers.Remove(bounceRenderers[i]);
+            Destroy(_renderer.gameObject);
+        }
+    }
+}
