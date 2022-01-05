950b3afab6910ff665caa852df1489ec0b644894
MrAladross <52119895+MrAladross@users.noreply.github.com>
2022-01-04 19:47:50 -0800

    Add files via upload

---
+++ CardClass/Card.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System;
+[Serializable]
+public class Card 
+{
+    public int _value;
+    public enum Suit { hearts, diamonds, clubs, spades };
+    public Suit _suit;
+
+    public Card()
+    {
+        _value = 1;
+        _suit = Suit.hearts;
+    }
+    public Card(int value, Suit suit)
+    {
+        _value = value;
+            _suit = suit;
+    }
+
+
+}
---
+++ CardClass/CardInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo : MonoBehaviour
+{
+    [SerializeField]
+    private Card card;
+    [SerializeField]
+    private Player player;
+
+    public CardInfo()
+    {
+        card = new Card();
+        player = new Player();
+    }
+
+    public void SetCard(Card c)
+    {
+        card = c;
+    }
+    public void SetPlayer(Player p)
+    {
+        player = p;
+    }
+}
